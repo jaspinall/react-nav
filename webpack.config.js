c0fee0a3557708092eca947a954b9462cb7a75bc
jaspinall <julia.stefanczyk@gmail.com>
2017-01-16 11:14:58 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index.js',
+  output: { path: __dirname + '/build', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
+};
